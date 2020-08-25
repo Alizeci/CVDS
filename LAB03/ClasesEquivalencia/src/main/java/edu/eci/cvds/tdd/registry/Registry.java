f7f09d0cda19b138d2d3756b99f03d353e0d68a2
Laura Alejandra Izquierdo <alizeci17@gmail.com>
2020-08-25 12:55:56 -0500

    Avance Registry

--- LAB03/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ LAB03/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,11 +1,25 @@
 package edu.eci.cvds.tdd.registry;
 
+import java.util.ArrayList;
+
+
 public class Registry {
+	private ArrayList<Person> personasRegistradas;
 	
 	public RegisterResult registerVoter(Person p) {
-
-        // TODO Validate person and return real result.
 		
-        return RegisterResult.VALID;
+		personasRegistradas = new ArrayList<Person>();
+		RegisterResult result = RegisterResult.VALID;
+		
+        // TODO Validate person and return real result.
+		if((p.getAge() >= 18 && p.getAge() < 150) && p.isAlive()== true) {
+			
+			for(Person n:personasRegistradas) {
+				if(n.getId() == p.getId()) {
+					result = RegisterResult.DUPLICATED;
+				}
+			}
+		}
+		return result;
     }
 }
